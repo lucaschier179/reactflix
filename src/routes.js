b21e4d7dd01385f1bfd47a4b4503f578d60f169c
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-06 12:24:16 -0300

    aula 62 - 06/10
    Carregando filmes

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,17 @@ import Header from './components/Header';
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 
+import Erro from './pages/Erro';
+
 function RoutesApp(){
     return(
         <BrowserRouter>
             <Header/>
             <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/filme/:id' element={<Filme/>}/>
+                <Route path='/' element={ <Home/> }/>
+                <Route path='/filme/:id' element={ <Filme/> }/>
+
+                <Route path='*' element={ <Erro/> } />
             </Routes>
         </BrowserRouter>
     )
