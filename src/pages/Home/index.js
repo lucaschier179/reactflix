fa18dc1a32c1a0245d4ad9062be7834cad3069f6
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-06 10:20:20 -0300

    aula 60 - 06/10
    Criando a nossa Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+// URL DA API = movie/now_playing?api_key=710272209fd57c108bd36dbb4f6000c2&language=pt-BR
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "710272209fd57c108bd36dbb4f6000c2",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return(
         <div>
             <h1>Bem Vindo a Home</h1>
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// BASE DA URL = https://api.themoviedb.org/3/ 
+// URL DA API = movie/now_playing?api_key=710272209fd57c108bd36dbb4f6000c2&language=pt-BR
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
