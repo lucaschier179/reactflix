e6c137dc796d148f8b177bfe157e643540f4fe9c
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-07 13:27:27 -0300

    aula 68 - 07/10
    Manipulando lista de filmes

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,9 +10,18 @@ function Favoritos(){
         setFilmes(JSON.parse(minhaLista) || [])
     }, [])
 
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((item) => {
+            return(item.id !== id)
+        })
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@ReactFlix", JSON.stringify(filtroFilmes))
+    }
+
     return(
         <div className='meus-filmes'>
             <h1>Meus filmes</h1>
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo...</span>}
             <ul>
                 {filmes.map((item) => {
                     return(
@@ -20,7 +29,7 @@ function Favoritos(){
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
