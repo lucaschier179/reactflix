6eac8abb769adc4999f7cc8ea1e50903a7734375
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-10 08:39:16 -0300

    aula 69  - 10/10
    Personalizando alertas do sistema e alterações no estilo.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import RoutesApp from "./routes";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer autoClose={3000}/>
       <RoutesApp/>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Hind&display=swap" rel="stylesheet"/>
     </div>
   );
 }
--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState} from 'react';
 import { useParams, useNavigate } from 'react-router-dom'
 import './filme-info.css';
 import api from '../../services/api';
+import { toast} from 'react-toastify';
 
 function Filme(){
     const { id } = useParams();
@@ -41,13 +42,13 @@ function Filme(){
 
         const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
         if(hasFilme){
-            alert("ESSE FILME JÁ FOI ADICIONADO EM MEUS FILMES");
+            toast.warn("Esse filme já está na sua lista!")
             return;
         }
 
         filmesSalvos.push(filme);
         localStorage.setItem("@ReactFlix", JSON.stringify(filmesSalvos));
-        alert("FILME SALVO COM SUCESSO")
+        toast.success("Filme salvo com sucesso!")
     }
 
     if(loading){
