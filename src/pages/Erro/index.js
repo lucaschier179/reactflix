2eaf840127969e1b4b39c058635872fccfc23e9e
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-11 13:27:24 -0300

    a

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -3,8 +3,8 @@ import './erro.css'
 
 function Erro(){
     return(
-        <div className="not-found">
-            <h1> ERRO 404</h1>
+        <div className="p-40 justify-center text-center text-lg text-white ">
+            <h1 className="text-9xl"> ERRO 404</h1>
             <h2>Página não encontrada!</h2>
             <Link to='/'>Veja todos os filmes!</Link>
         </div>
