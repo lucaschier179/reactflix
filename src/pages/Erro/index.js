6f401921eb48f9097420b49987f1b90056a26054
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-13 11:46:56 -0300

    att da page erro - 13/10
    Criação do estilo da página erro 404 através do tailwind

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom"
-import './erro.css'
 
 function Erro(){
     return(
-        <div className="p-40 justify-center text-center text-lg text-white ">
+        <div className="h-[80vh] flex flex-col items-center justify-center text-center text-lg text-white ">
             <h1 className="text-9xl"> ERRO 404</h1>
-            <h2>Página não encontrada!</h2>
-            <Link to='/'>Veja todos os filmes!</Link>
+            <h2 className="text-2xl">Página não encontrada!</h2>
+            <Link className="bg-[#FFFC00] rounded-md mt-3 p-1 h-10 w-60 text-black text-2xl" to='/'>Veja todos os filmes!</Link>
         </div>
     )
 }
