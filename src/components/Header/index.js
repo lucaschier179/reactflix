770d79f7a4106793bea575aa680c9ea8aa1d4e21
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-13 13:23:17 -0300

    Classes no tailwind (incompleto) 13/10

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,12 @@
-import './header.css';
+//import './header.css';
 import { Link } from 'react-router-dom';
 
 function Header(){
     return(
-        <header>
-            <Link className='logo' to='/'>ReactFlix</Link>
-            <body>
-            <Link className='favoritos' to='/favoritos'>Meus filmes</Link>
-            <Link className='top20Filmes' to='/top20filmes'>Top 20 Filmes</Link>
-            </body>
+        <header className='w-full h-16 bg-[#FFFC00] flex items-center justify-around'>
+            <Link className='text-4xl font-bold' to='/'>ReactFlix</Link>
+            <Link className='botao' to='/favoritos'>Meus filmes</Link>
+            <Link className='botao' to='/top20filmes'>Top 20 Filmes</Link>
         </header>
     )
 }
